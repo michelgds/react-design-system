ca41be9c9996b7edaa7935e7032f9a831ad27707
Michel Gracia <michel.gracia@thoughtworks.com>
2022-10-14 10:51:00 -0300

    Implement the app login page

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,14 +1,12 @@
 import * as CheckboxPromitive from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps {
+export interface CheckboxProps extends CheckboxPromitive.CheckboxProps {}
 
-}
-
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
   return (
     <CheckboxPromitive.Root 
-      className='w-6 h-6 p-[2px] bg-gray-800 rounded'
+      className='w-6 h-6 p-[2px] bg-gray-800 rounded' {...props}
     >
       <CheckboxPromitive.Indicator asChild>
         <Check weight='bold' className='h5 w-5 text-cyan-500'/>
